a1ce8adf93e7877ba51c92467bfb758d89d8ad28
hari.mopuri <ubuntu@ip-172-31-87-124.ec2.internal>
2019-09-21 05:53:32 +0000

    time added

--- demo/test.py
+++ demo/test.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+from datetime import datetime
+now = datetime.now()
+current_time = now.strftime("%H:%M:%S")
+print("Current Time =", current_time)
 import psutil
 # gives a single float value
 print "cpu"
